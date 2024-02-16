6003798dff1011c4bfb0bc2a5b48062b6276e7c2
Allen Chen <wen900445@gmail.com>
2024-02-16 17:31:36 +0800

    [frontend] feat: init basic layout

---
+++ frontend/src/layouts/MainLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-layout view="hHh LpR fFf">
+    <q-header elevated class="row justify-start items-center q-pa-xs glossy">
+      <q-icon class="q-ml-sm q-py-xs" name="fa-regular fa-envelope" size="md" />
+      <div class="q-ml-md text-h6">Batch message postman</div>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+
+</script>
---
+++ frontend/src/pages/IndexPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page :style-fn="tweakPageH" class="q-pa-lg bg-light-blue-1">
+    <div class="fit column">
+      <div class="col-10 q-pa-sm">
+        <div class="fit row">
+        </div>
+      </div>
+      <div class="col-2 q-px-sm">
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+
+function tweakPageH(offset) {
+  return { height: offset ? `calc(100vh - ${offset}px)` : '100vh' };
+}
+
+</script>
